bca02e37fc99d62754b8a06818da8bfdaab9e16f
vinicozzi <vinicozzimincent@gmail.com>
2023-04-24 16:22:07 -0400

    help me

--- skeleton 2/bin/p01_basic_server.rb
+++ skeleton 2/bin/p01_basic_server.rb
@@ -1,1 +1,23 @@
 require 'rack'
+
+app =  Proc.new do |env|
+    ['200',{"Content-Type"=>'text/html'}, ['Hello, Rok']]
+    # req = Rack::Request.new(env)
+    # res = Rack::Request.new 
+    # res['Content-Type'] = 'text/html'
+    # res.write['Hello Arvid!']
+    # res.finish 
+end 
+
+# app = Proc.new do |env|
+#     req = Rack::Request.new(env)
+#     res = Rack::Response.new
+#     MyController.new(req, res).execute
+#     res.finish
+#   end
+
+
+Rack::Server.start(
+    app: app, 
+    Port: 3000
+)
